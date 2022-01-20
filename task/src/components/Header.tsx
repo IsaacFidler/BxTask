d44267986bcb18d1aea560bdfb2919ae492f8697
IsaacFidler <zac.fidler2@gmail.com>
2022-01-20 17:14:31 +0000

    feat: header

--- task/src/components/Header.tsx
+++ task/src/components/Header.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+//styles
+import styles from '../styles/Header.module.scss';
 
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div></div>;
+  return (
+    <div className={styles.headerContainer}>
+      <h1 className={styles.headerSelected}>Selected:</h1>
+    </div>
+  );
 }
